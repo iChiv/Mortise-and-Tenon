4f6440562399f6911cceb4a32064d0c82c57ac01
LAPTOP-PMIKHCHE\xiyom <marcus_yvelin@163.com>
2024-09-03 22:29:17 +0100

    Update for the exhibition

--- Assets/Scripts/XR/PoemTrigger.cs
+++ Assets/Scripts/XR/PoemTrigger.cs
@@ -1,5 +1,6 @@
 using DG.Tweening;
 using UnityEngine;
+using UnityEngine.SceneManagement; // Add this line to use SceneManager
 
 namespace XR
 {
@@ -22,11 +23,24 @@ namespace XR
             {
                 nextObjectSpriteRenderer.DOFade(1, fadeDuration).OnComplete(() =>
                 {
-                    // 显示一段时间后渐隐
+                    // Display for a specified duration then fade out
                     DOVirtual.DelayedCall(displayDuration, () =>
                     {
-                        nextObjectSpriteRenderer.DOFade(0, fadeDuration);
+                        nextObjectSpriteRenderer.DOFade(0, fadeDuration).OnComplete(CheckAndSwitchScene);
                     });
+                });
+            }
+        }
+
+        private void CheckAndSwitchScene()
+        {
+            // Assuming "资源 23" is a sprite reference or identifier you have
+            if (nextObjectSpriteRenderer.name == "资源 23")
+            {
+                // Wait for 5 seconds before switching scenes
+                DOVirtual.DelayedCall(5f, () =>
+                {
+                    SceneManager.LoadScene("GameMainMenuTT");
                 });
             }
         }
